39a8e78ea9fb201c8dcaa16798c95b42f5a12ed5
O Nacélio <86494087+onacelio@users.noreply.github.com>
2022-01-25 11:59:41 -0300

    Adicionado novas funções e inicio do desafio

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,9 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components'
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json'
 
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-            * {
-                margin: 0;
-                padding: 0;
-                box-sizing: border-box;
-                list-style: none;
-            }
 
-            body {
-                font-family: 'Open Sans', sans-serif;
-            }
-
-            html, body, #__next {
-                min-height: 100vh;
-                display: flex;
-                flex: 1;
-            }
-            #__next {
-                flex: 1;
-            }
-
-            #__next > * {
-                flex: 1;
-            }
-        `}</style>
-    )
-}
 
 function Titulo(props) {
     const Tag = props.tag || 'h1';
@@ -63,11 +37,12 @@ export default HomePage
 */
 
 export default function PaginaInicial() {
-    const username = 'onacelio';
+    //const username = 'onacelio';
+    const [username, setUsername] = React.useState('onacelio');
+    const roteamento = useRouter();
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -96,17 +71,39 @@ export default function PaginaInicial() {
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function (infosDoEvento) {
+                            infosDoEvento.preventDefault();
+                            console.log('Alguém tentou entrar')
+                            roteamento.push('/chat');
+                            // window.location.href = "/chat"
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
                         }}
                     >
-                        <Titulo tag="h2">Bem-Vindo ao nosso Server!</Titulo>
+                        <Titulo tag="h2">Bem-Vindo ao servidor!</Titulo>
                         <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                             {appConfig.name}
                         </Text>
 
+                        {/* <input type="text"
+                        value={username}
+                        onChange={function Handler(event) {
+                            console.log('usuário digitou', event.target.value)
+                            //Onde tá o valor?
+                            const valor = event.target.value;
+                            //Trocar o valor da variável
+                            // Atráves do react e avisa quem precisa
+                            setUsername(valor);
+                        }}/> */}
+                        {/* Caso eu coloque dois caracteres no input a imagem não irá aparecer. "p" e "pe" são exemplos */}
                         <TextField
+                            value={username}
+                            onChange={function Gandler(event) {
+                                const valor = event.target.value;
+                                setUsername(valor);
+                            }}
                             fullWidth
                             textFieldColors={{
                                 neutral: {
